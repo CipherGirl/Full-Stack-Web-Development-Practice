41bb85fb0c8cf1a3df5328d11bc3d1a80f37549e
CipherGirl <ciphergirlmeow@gmail.com>
2020-09-23 12:25:31 +0600

    Complete

--- JavaScript Course/Pig Game/app.js
+++ JavaScript Course/Pig Game/app.js
@@ -9,3 +9,84 @@ GAME RULES:
 
 */
 
+var scores, roundScore, activePlayer, dice, gameplaying;
+
+init();
+
+document.querySelector(".btn-roll").addEventListener("click", () => {
+  if (gameplaying) {
+    dice = Math.floor(Math.random() * 6) + 1;
+    const diceDom = document.querySelector(".dice");
+
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + dice + ".png";
+    if (dice !== 1) {
+      roundScore += dice;
+      document.getElementById(
+        "current-" + activePlayer
+      ).textContent = roundScore;
+      if (scores[activePlayer] + roundScore >= 20) {
+        gameOver();
+      }
+    } else {
+      nextPlayer();
+    }
+  }
+});
+
+document.querySelector(".btn-hold").addEventListener("click", () => {
+  if (gameplaying) {
+    scores[activePlayer] += roundScore;
+    document.getElementById(`score-${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 20) {
+      gameOver();
+    } else {
+      nextPlayer();
+    }
+  }
+});
+
+document.querySelector(".btn-new").addEventListener("click", init);
+
+const gameOver = () => {
+  document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+  document.querySelector(".dice").style.display = "none";
+  document
+    .querySelector(".player-" + activePlayer + "-panel")
+    .classList.add("winner");
+  document
+    .querySelector(".player-" + activePlayer + "-panel")
+    .classList.remove("active");
+  gameplaying = false;
+};
+
+const nextPlayer = () => {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+  document.getElementById("current-0").textContent = 0;
+  document.getElementById("current-1").textContent = 0;
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+  document.querySelector(".dice").style.display = "none";
+};
+
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gameplaying = true;
+
+  document.querySelector(".dice").style.display = "none";
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+}
